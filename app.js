8e98e36366bc7c6c7ea5f401859bd984b310291d
Yosua Winata <yosua.winata@gmail.com>
2023-10-28 23:24:08 +0700

    add vuejs style

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const listEl = document.querySelector('ul');
+// Manual javascript without Vue
 
-function addGoal() {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li')
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value = '';
-}
+// const inputEl = document.querySelector('input');
+// const buttonEl = document.querySelector('button');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li')
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
+// }
 
 
-buttonEl.addEventListener('click', addGoal)
+// buttonEl.addEventListener('click', addGoal)
+
+// javascript with vuejs
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');

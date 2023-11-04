8bcf0c79c7087b8eb7efa71de150736e27886ad6
Yosua Winata <yosua.winata@gmail.com>
2023-11-04 10:06:40 +0700

    add h3 style in css file, update README file, and add function v-directive and function in app.js file and index.html

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # learnjs
-This repo is purposed to learn js, both native js or using framework. The framework will use vuejs.
+This repo is purposed to learn js, both native js and using framework. The framework will use vuejs.
 
 # ch_14
-This chapter will learn about basics of vuejs.
+This chapter will learn about basics of vuejs. Creating and connecting vue app instances.
+
+# ch_15
+You will learn about data binding and interpolation.
+
+# ch_16
+You will learn about Binding Attributes with the v-bind directive.
+
+# ch_17
+You will learn about "methods" in VueJs App.
+
+# ch_18
+You will learn about working with data inside of a vue app.
+
+# ch_19
+You will learn about outputting raw HTML content with v-html.
+
--- app.js
+++ app.js
@@ -16,17 +16,45 @@
 // buttonEl.addEventListener('click', addGoal)
 
 // javascript with vuejs
-Vue.createApp({
+// Vue.createApp({
+//     data() {
+//         return {
+//             goals: [],
+//             enteredValue: ''
+//         };
+//     },
+//     methods: {
+//         addGoal() {
+//             this.goals.push(this.enteredValue);
+//             this.enteredValue = '';
+//         }
+//     }
+// }).mount('#app');
+
+// javascript basic - section 2 - ch_14
+const app = Vue.createApp({
     data() {
         return {
-            goals: [],
-            enteredValue: ''
+            // courseGoal: 'Finish the course and well understand about vue!',
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: '<h3>Master Vue and build amazing apps!</h3>',
+            // courseGoalB: 'Master Vue and build amazing apps!',
+            vueLink: 'https://vuejs.org/'
         };
     },
     methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                // return 'Learning Vue!';
+                return this.courseGoalA;
+            } else {
+                // return 'Mastering Vue!'
+                return this.courseGoalB;
+            }
         }
     }
-}).mount('#app');
+});
+
+// mounting to the index
+app.mount('#user-goal')

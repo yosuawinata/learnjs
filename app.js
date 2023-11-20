3a3c8d3d62fd98e6a5170de2423344f1955e518e
Yosua Winata <yosua.winata@gmail.com>
2023-11-20 17:46:17 +0700

    working with watchers

--- app.js
+++ app.js
@@ -4,18 +4,62 @@ const app = Vue.createApp({
             counter: 0,
             name: '',
             // the data property to show if the field name is hit by enter (listening on keyboard event)
-            confirmedName: '' 
+            confirmedName: '',
+            // we can remark fullname variabel first (usage of watchers)
+            // because this variable is used too with computed.
+            // fullname: '',
+            lastName: ''
         }
+    },
+    watch: {
+        // watch more powerfull if use in this case like this
+        // compare if this counter is used by computed property, there is will problem
+        // this task that make watcher more shine
+        // watchers will use for HTTP request when data changed or timer
+        counter(value) {
+            if (value > 50) {
+                // this will not work, because this is used by local variable
+                // must declare the const
+                const that = this;
+
+                // set timer
+                setTimeout(function() {
+                    that.counter = 0;
+                    // this.counter = 0;
+                }, 2000); // set timer for 2s from the condition is fullfilled (greater than 50)
+                // this.counter = 0;
+            }
+        }
+        // we can use watcher using given file name, for example param name.
+        // name(value) {
+        //     if (value === '') {
+        //         this.fullname = '';
+        //     }
+        //     else {
+        //         // this.fullname = this.name + ' ' + 'Winz';
+        //         // event in above can be replaced with the latest value
+        //         this.fullname = value + ' ' + this.lastName;    
+        //     }
+        // },
+        // // if we 're using two param, usually that is not eficient. because a lot of code.
+        // lastName(value) {
+        //     if (value === '') {
+        //         this.lastName = '';
+        //     } else {
+        //         this.fullname = this.name + ' ' + value;
+        //     }
+        // }
     },
     // this property like methods, but this is only re-executed if there any updated data dependencies
     computed: {
         // we treat the computed properties like data, not a like function
+        // comment first when we want to use watchers
         fullname() {
             console.log('Running again from computed...');
-            if (this.name === '') {
+            if (this.name === '' || this.lastName === '') {
                 return '';
             }
-            return this.name + ' ' + 'Winz';
+            return this.name + ' ' + this.lastName;
         }
     },
     methods: {
@@ -66,6 +110,7 @@ const app = Vue.createApp({
             // vanillaJs to make function for reset input button
             // document.querySelector('input').value = '';
             this.name = '';
+            this.lastName = '';
         }
     }
 });
